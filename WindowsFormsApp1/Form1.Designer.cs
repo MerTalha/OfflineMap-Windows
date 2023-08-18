966aac2b05509b90e146e66fa6cbacd8d021dfd0
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2023-08-18 11:34:50 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMap2 = new AxMapWinGIS.AxMap();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMap2
+            // 
+            this.axMap2.Enabled = true;
+            this.axMap2.Location = new System.Drawing.Point(209, 75);
+            this.axMap2.Name = "axMap2";
+            this.axMap2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap2.OcxState")));
+            this.axMap2.Size = new System.Drawing.Size(924, 459);
+            this.axMap2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1327, 676);
+            this.Controls.Add(this.axMap2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axMap2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private AxMapWinGIS.AxMap axMap2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using MapWinGIS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int providerid;
+        TileProviders providers;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            providers = axMap2.Tiles.Providers;
+            providerid = (int)tkTileProvider.ProviderCustom + 1001;
+
+            providers.Add(providerid, "map", "C:/ms4w/Apache/htdocs/sat/z{zoom/{y}/{x}.jpg", tkTileProjection.SphericalMercator, 0, 10);
+
+            axMap2.Tiles.ProviderId = providerid;
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
