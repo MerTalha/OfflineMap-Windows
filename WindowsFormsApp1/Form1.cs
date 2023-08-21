b46acc31daa330d37aba604d9244b01042b67a77
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2023-08-21 11:23:04 +0300

    Update

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -27,6 +27,35 @@ namespace WindowsFormsApp1
             
         }
 
+        private void AxMap2MouseDownEvet(object sender, _DMapEvents_MouseDownEvent e)
+        {
+            int m_layerHandle = -1;
+            Shapefile sf = axMap2.get_Shapefile(m_layerHandle);
+
+            axMap2.CursorMode = MapWinGIS.tkCursorMode.cmMeasure;
+            String loc = MapWinGIS.tkCursorMode.cmMeasure.ToString();
+
+            MapWinGIS.Point pnt = new MapWinGIS.Point();
+            double x = 0;
+            double y = 0;
+            axMap2.PixelToProj(0, 0, ref x, ref y);
+            pnt.x = e.x; pnt.y = e.y;
+            Shape shp = new Shape();
+            shp.Create(ShpfileType.SHP_POINT);
+            int index = shp.numPoints;
+            shp.InsertPoint(pnt, index);
+            String a = pnt.x.ToString();
+            String b = pnt.y.ToString();
+            Console.WriteLine(a +  " "  + b); 
+            //index = sf.NumShapes;
+            /*if (!sf.EditInsertShape(shp, ref index))
+            {
+                MessageBox.Show("Failed to insert shape: " + sf.ErrorMsg[sf.LastErrorCode]);
+                return;
+            }*/
+            axMap2.Redraw();
+        }
+
         private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
         {
 
@@ -52,40 +81,10 @@ namespace WindowsFormsApp1
             axMap2.CursorMode = MapWinGIS.tkCursorMode.cmZoomOut;
         }
 
-        public void Axmap2MouseDownEvet(object sender, _DMapEvents_MouseDownEvent e)
-        {
-
-        }
         private void button5_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void marker(object sender, _DMapEvents_MouseDownEvent e)
-        {
-            int m_layerHandle = -1;
-            Shapefile sf = axMap2.get_Shapefile(m_layerHandle);
-
-            axMap2.CursorMode = MapWinGIS.tkCursorMode.cmMeasure;
-            String loc = MapWinGIS.tkCursorMode.cmMeasure.ToString();
-
-            MapWinGIS.Point pnt = new MapWinGIS.Point();
-            double x = 0;
-            double y = 0;
-            axMap2.PixelToProj(e.x, e.y, ref x, ref y);
-            pnt.x = x; pnt.y = y;
-            Shape shp = new Shape();
-            shp.Create(ShpfileType.SHP_POINT);
-            int index = shp.numPoints;
-            shp.InsertPoint(pnt, index);
-
-            index = sf.NumShapes;
-            if (!sf.EditInsertShape(shp, ref index))
-            {
-                MessageBox.Show("Failed to insert shape: " + sf.ErrorMsg[sf.LastErrorCode]);
-                return;
-            }
-            axMap2.Redraw();
+            axMap2.SendMouseDown = true;
+            axMap2.MouseDownEvent += AxMap2MouseDownEvet;
         }
 
         private void button6_Click(object sender, EventArgs e)
